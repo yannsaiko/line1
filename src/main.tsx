e1b9f146e1dff60005b01f76930beb57de6b8743
yannsaiko <53604258+yannsaiko@users.noreply.github.com>
2026-09-12 23:20:08 +0900

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ChatApp from './ChatApp';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChatApp />
-  </React.StrictMode>
-);
