faab7f6fd5e11a8bbc4e9d3e37667018abe83de9
yannsaiko <53604258+yannsaiko@users.noreply.github.com>
2026-09-15 10:43:07 +0900

    Create lineDatabase.ts

---
+++ src/types/lineDatabase.ts
@@ -0,0 +1,60 @@
+export interface RawZUser {
+  ZMID?: string;
+  zmid?: string;
+  ZNAME?: string;
+  zname?: string;
+  ZCUSTOMNAME?: string;
+  zcustomname?: string;
+  [key: string]: any;
+}
+
+export interface RawZMessage {
+  Z_PK?: number;
+  z_pk?: number;
+  ZTEXT?: string;
+  ztext?: string;
+  ZCREATEDTIME?: number | string;
+  zcreatedtime?: number | string;
+  ZSENDER?: string;
+  zsender?: string;
+  ZSENDERHEADER?: string;
+  zsenderheader?: string;
+  ZCHAT?: number | string;
+  zchat?: number | string;
+  [key: string]: any;
+}
+
+export interface RawZChat {
+  Z_PK?: number;
+  z_pk?: number;
+  ZMID?: string;
+  zmid?: string;
+  ZNAME?: string;
+  zname?: string;
+  [key: string]: any;
+}
+
+export interface NormalizedUser {
+  mid: string;
+  displayName: string;
+  customName?: string;
+  resolvedName: string;
+}
+
+export interface NormalizedMessage {
+  id: string | number;
+  text: string;
+  senderMid: string;
+  senderName: string;
+  timestamp: number;
+  formattedTime: string;
+  formattedFullDate: string;
+  isMyMessage: boolean;
+}
+
+export interface NormalizedChatRoom {
+  chatMid: string;
+  roomTitle: string;
+  partner: NormalizedUser | null;
+  messages: NormalizedMessage[];
+}
